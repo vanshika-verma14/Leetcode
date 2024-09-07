91f227a486701407d49530047277b2b5b022080c
vanshika-verma14 <vanshika9994verma@gmail.com>
2024-09-07 20:35:14 +0530

    twosum leetcode question

---
+++ Twosum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Twosum
+{
+
+    public static int[] twoSum(int[] arr, int tg , int n) {
+        for (int i = 0; i < n ; i++) {
+         for (int j = i+1 ; j < n ; j++) {
+            if(arr[i]+arr[j] == tg )
+            return new int[]{i,j};
+         }
+        } 
+        return null;
+}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Number of elements in array - ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();    
+        }
+        System.out.println("Enter the target number - ");
+        int tg = sc.nextInt();
+        int[] tgarr= twoSum(arr, tg ,n );
+        System.out.println("New array of indices " + Arrays.toString(tgarr) );
+
+    }
+}
