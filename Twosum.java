0a4c87eb6946c5a7c494953272ace3970dd75d07
Vanshika Verma <155113948+vanshika-verma14@users.noreply.github.com>
2026-07-11 23:56:07 +0530

    Update Twosum.java

--- Twosum.java
+++ Twosum.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.util.Arrays;
 class Twosum
 {
-
     public static int[] twoSum(int[] arr, int tg , int n) {
         for (int i = 0; i < n ; i++) {
          for (int j = i+1 ; j < n ; j++) {
